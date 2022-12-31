7a375e82809a8c96dbdd814b911d2b313a4527d5
vnguyenlong <nvlong567@gmail.com>
2022-12-31 14:35:27 +0700

    [++] fix server

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const dotenv = require('dotenv');
+// const path = require('path');
+dotenv.config({path: '.env'})
+
 
 const app = express();
 
 app.use(cors());
 app.options('*', cors());
 
-
 // Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '2mb' }));
 app.use(morgan('tiny'));
 
 // Connect DB
-mongoose.connect(process.env.DB_NAME, {
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/datas', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => { console.log('Database Connection is ready...')}).catch((err) => { console.error(err)});
 
 // Server
-const port = process.env.SERVER_PORT || 5000;
-app.listen(port, () => {
-    console.log(`server is running ${port}`);
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`server is running ${PORT}`);
 })
